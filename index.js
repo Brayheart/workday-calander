8b3983b8005bc0e72c45c3e047ad08c59c3282ea
Tyler Bray <broncobray77@gmail.com>
2021-07-23 16:28:58 -0700

    added local storage

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 $( document ).ready(function() {
+  var notes = JSON.parse(localStorage.getItem('notes')) || {
+    "9AM": '',
+    "10AM": '',
+    "11AM": '',
+    "12PM": '',
+    "1PM": '',
+    "2PM": '',
+    "3PM": '',
+    "4PM": '',
+    "5PM": '',
+  } 
+
+for (const property in notes) {
+  console.log(`${property}: ${notes[property]}`);
+  $(`#${property}`)[0].value = notes[property]
+}
+
   $(".fa-save").click(function(event){
-    console.log($(event.target).parent().parent().prev().children().val())
+    var text = $(event.target).parent().parent().prev().children().val()
+    var time = $(event.target).parent().parent().prev().prev().children().text()
+    notes[time] = text
+    localStorage.setItem('notes', JSON.stringify(notes))
+    console.log(notes)
   })
+
 });
