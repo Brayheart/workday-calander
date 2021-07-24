ada4409a4a2933618772848a9866e7df3966fd61
Tyler Bray <broncobray77@gmail.com>
2021-07-23 17:19:43 -0700

    working

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
+  var hour = moment().format("h");
+
+  $('#date').text(moment().format("dddd, MMMM Do YYYY"))
   var notes = JSON.parse(localStorage.getItem('notes')) || {
     "9AM": '',
     "10AM": '',
@@ -13,15 +16,22 @@ $( document ).ready(function() {
 
 for (const property in notes) {
   console.log(`${property}: ${notes[property]}`);
+  // var timeblock = property.slice(0, property.length - 2)
+  // console.log(timeblock, hour)
+
+
+  // console.log($(property))
   $(`#${property}`)[0].value = notes[property]
 }
+
+console.log($('.form-control'))
 
   $(".fa-save").click(function(event){
     var text = $(event.target).parent().parent().prev().children().val()
     var time = $(event.target).parent().parent().prev().prev().children().text()
     notes[time] = text
     localStorage.setItem('notes', JSON.stringify(notes))
-    console.log(notes)
+    // console.log(notes)
   })
 
 });
